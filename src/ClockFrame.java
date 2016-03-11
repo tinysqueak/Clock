6650c4f8dc7bc887900f52fa11503b33fa3ce5fc
Willy Yang <tinysqueak@gmail.com>
2016-03-10 22:10:46 -0800

    Added seconds hand

--- src/ClockFrame.java
+++ src/ClockFrame.java
@@ -8,19 +8,19 @@ public class ClockFrame extends JFrame{
 	public ClockFrame() {
 		
 		setSize(700, 600);
-		//setBackground(new Color(0x87ceeb));
-		repaint();
 		
 	}
 	
 	public void paint(Graphics g) {
 		
-		int targetX = 335 + (int)(125 * Math.cos(Math.toRadians(90 - 6*seconds)));
-		int targetX2 = 335 + (int)(50 * Math.cos(Math.toRadians(90 - seconds / 10)));
+		int targetSecondsX = 335 + (int)(125 * Math.cos(Math.toRadians(90 - 6 * seconds)));
+		int targetSecondsY = 275 - (int)(125 * Math.sin(Math.toRadians(90 - 6 * seconds)));
 		
-		int targetY = 275 - (int)(125 * Math.sin(Math.toRadians(90 - 6*seconds)));
-		int targetY2 = 275 - (int)(50 * Math.sin(Math.toRadians(90 - seconds / 10)));
+		int targetMinutesX = 335 + (int)(100 * Math.cos(Math.toRadians(90 - 6 * seconds / 60)));
+		int targetMinutesY = 275 - (int)(100 * Math.sin(Math.toRadians(90 - 6 * seconds / 60)));
 		
+		int targetHoursX = 335 + (int)(50 * Math.cos(Math.toRadians(90 - 30 * seconds / 3600)));
+		int targetHoursY = 275 - (int)(50 * Math.sin(Math.toRadians(90 - 30 * seconds / 3600)));
 		
 		try {
 			Thread.sleep(1000);
@@ -32,12 +32,14 @@ public class ClockFrame extends JFrame{
 		g.setColor(Color.BLACK);
 		g.drawOval(210, 150, 250, 250);
 		
-		drawMinuteHand(g, targetX, targetY);
-		drawHourHand(g, targetX2, targetY2);
+		drawSecondHand(g, targetSecondsX, targetSecondsY);
+		drawMinuteHand(g, targetMinutesX, targetMinutesY);
+		drawHourHand(g, targetHoursX, targetHoursY);
 		
 		seconds++;
 		System.out.println(seconds);
 		
+		if(seconds <= 3600)
 		repaint();
 		
 	}
@@ -46,6 +48,13 @@ public class ClockFrame extends JFrame{
 		
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 700, 600);
+		
+	}
+	
+	public void drawSecondHand(Graphics g, int targetX, int targetY) {
+		
+		g.setColor(Color.RED);
+		g.drawLine(335, 275, targetX, targetY);
 		
 	}
 	
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		ClockFrame clocks = new ClockFrame();
-		clocks.setDefaultCloseOperation(clocks.EXIT_ON_CLOSE);
+		clocks.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		clocks.setVisible(true);
 		clocks.setLocationRelativeTo(null);
 		clocks.setTitle("Clocks");
